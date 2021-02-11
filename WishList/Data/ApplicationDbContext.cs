63c6d18bf4d35afb6daee638d81e05b5a57dd259
Adrian Rhodd <Adrian.Rhodd@ioausa.com>
2021-02-11 09:41:32 -0500

    Completed module 4

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
